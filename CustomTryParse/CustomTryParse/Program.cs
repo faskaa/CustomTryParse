4ade60af8170d6b97f42fea32e110a03530ede50
faskaa <fazilnb@code.edu.az>
2023-08-29 14:32:21 +0400

    first commit

---
+++ CustomTryParse/CustomTryParse/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace CustomTryParse
+{
+   
+   
+
+    internal class Program
+    {
+        public static bool MyTryParse(out int a)
+        {
+            try
+            {
+                a= int.Parse("123");
+                Console.WriteLine(true);
+                return true;
+               
+            }
+            catch (Exception ex)
+            {
+
+                a = 0;
+                Console.WriteLine(false);
+                return false;
+                
+
+            }
+            
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            
+        } 
+    }
+}
